d552d0029044bc76d48734d1550257b38a608edf
Recesses-2007 <3617492651@qq.com>
2026-01-21 22:56:14 +0800

    feat(core) : 新增初始界面与清屏，输入函数

--- SMS.cpp
+++ SMS.cpp
@@ -16,10 +16,77 @@
 #include<ctime>
 #include<algorithm>
 #include<ranges>
-
 using namespace std;
 
-int main(){
+string cinLineString(void);
+void printOriginChoice(void);
+void originLogin(void);
+void registerAccount(void);
+void studentLogin(void);
+void cleanScreen(void);
+void sleepClean(void);
+
+void sleepClean(void){
+    Sleep(700);
+    system("cls");
+}
+string cinLineString(void){
+    string lineAnswer = "";
+    getline(cin, lineAnswer);
+    return lineAnswer;
+}
+
+void cleanScreen(void){
+    system("cls");
+}
+
+void registerAccount(void){
     
+}
+
+void studentLogin(void){
+
+}
+
+void printOriginChoice(void){
+    printf("       学生管理系统\n");
+    printf("-----------****-----------\n");
+    printf("1.注册账号     2.学生登陆\n");
+    printf("3.教师登陆     4.管理员登陆\n");
+    printf("5.账号修改     6.密码找回\n");
+    printf("7.退出系统\n");
+    printf("-----------****-----------\n");
+    cout << "请输入执行的操作" << endl;
+    string firstChoice = cinLineString();
+    cleanScreen();
+    if ("1" == firstChoice) {
+        registerAccount();
+    } else if ("2" == firstChoice) {
+        studentLogin();
+    } else if ("3" == firstChoice) {
+
+    } else if ("4" == firstChoice) {
+
+    } else if ("5" == firstChoice) {
+
+    } else if ("6" == firstChoice) {
+
+    } else if ("7" == firstChoice) {
+        
+        cout << "感谢使用学生管理系统" << endl;
+        exit(0);
+    } else {
+        cout << "请输入正确的操作序号！" << endl;
+        sleepClean();
+    }
+}
+void originLogin(void){
+    while (1) {
+        printOriginChoice();
+    }
+}
+
+int main(){
+    originLogin();
     return 0;
 }
