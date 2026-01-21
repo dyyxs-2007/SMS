fa32b853f7e0358c4b5aa3e31784bdc0a889e7a8
Recesses-2007 <3617492651@qq.com>
2026-01-21 21:43:59 +0800

    feat(utils) : 新增头文件

--- SMS.cpp
+++ SMS.cpp
@@ -1,4 +1,22 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<string>
+#include<Windows.h>
+#include<conio.h>
+#include<vector>
+#include<map>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<queue>
+#include<deque>
+#include<cstdio>
+#include<cmath>
+#include<cstring>
+#include<cstdlib>
+#include<ctime>
+#include<algorithm>
+#include<ranges>
+
 using namespace std;
 
 int main(){
