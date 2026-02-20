b5f3c57a7e9db36a29c3dafd7fe15732f1007d26
Recesses-2007 <3617492651@qq.com>
2026-02-20 21:33:06 +0800

    feat(core)：新增初始化链表 学生信息，老师信息，管理员信息的文件读入 新增管理员的节点初始化

--- src/SMS.cpp
+++ src/SMS.cpp
@@ -13,7 +13,6 @@
 #include<algorithm>
 #include<ranges>
 #include<fstream>
-#define INPUT_MAX_SIZE 20
 using namespace std;
 
 string cinLineString(void);
@@ -23,8 +22,6 @@ void registerAccount(void);
 void studentLogin(void);
 void cleanScreen(void);
 void sleepClean(void);
-void allAccountRead(void);
-void accountRead(int flag, char* path);
 void teacherLogin(void);
 void studentOption(void);
 bool existPassWordNode(int flag, string passWord);
@@ -38,21 +35,22 @@ struct AccountNode{//账号密码节点定义
 };
 struct StudentsNode{//学生信息节点定义
     StudentsNode* next;
+    StudentsNode* prev;
     string name;
-    string che;
-    string mat;
-    string eng;
+    string score;
     string number;
     string clss;
-    StudentsNode() : next(NULL) {}
+    StudentsNode() : next(NULL), prev(NULL) {}
+    StudentsNode(string s1, string s2, string s3, string s4) : next(NULL), prev(NULL), name(s1), score(s2), number(s3), clss(s4) {}
 };
 
-struct TeachersNode{
+struct TeachersNode{//老师信息节点定义
     TeachersNode* next;
     string name;
     string number;
     string clss;
     TeachersNode() : next(NULL) {}
+    TeachersNode(string s1, string s2, string s3) : next(NULL), name(s1), number(s2), clss(s3) {}
 };
 
 ////////////
@@ -63,8 +61,129 @@ AccountNode* studentAccountHead = new AccountNode();//学生账号链表头节点
 AccountNode* teacherAccountHead = new AccountNode();//教师账号链表头节点
 AccountNode* AdminAccountHead = new AccountNode();//管理员账号链表头节点
 ////////////账号链表读取
-void allAccountRead(void) {
-    //...
+void teacherNodeRead(void) {//老师细节信息节点读取
+    ifstream ifs("C:\\Users\\dyyxs\\Desktop\\SMS\\date\\TeacherIP.bin",  ios::binary);
+    if (!ifs) {
+        cout << "打开文件失败" << endl;
+        exit(1);
+    }
+    int multiple = 0;
+    ifs.read((char *)&multiple, sizeof(int));
+    if (multiple > 0) {
+        for (int i = 0; i < multiple; i++) {
+            int length;
+            ifs.read((char*)&length, sizeof(int));
+            string name(length, '\0');
+            ifs.read((char*)&name[0], length);
+            ifs.read((char*)&length, sizeof(int));
+            string number(length, '\0');
+            ifs.read((char*)&number[0], length);
+            ifs.read((char*)&length, sizeof(int));
+            string clss(length, '\0');
+            ifs.read((char*)&clss[0], length);
+            TeachersNode* temp = new TeachersNode(name, number, clss);
+            TeachersNode* help = teacherHead;
+            while (NULL != help->next) {
+                help = help->next;
+            }
+            help->next = temp;
+            if(!ifs) {
+                cout << "读取文件失败" << endl;
+                exit(1);
+            }
+        }
+    }
+    ifs.close();
+}
+void accountRead(int flag) {//所有账号节点读取
+    string path;
+    AccountNode* head = NULL;
+    if (flag == 1) {//学生
+        head = studentAccountHead;
+        path = "C:\\Users\\dyyxs\\Desktop\\SMS\\date\\StudentDate.bin";
+    } else if (flag == 0) {//老师
+        head = teacherAccountHead;
+        path = "C:\\Users\\dyyxs\\Desktop\\SMS\\date\\TeacherDate.bin";
+    } else {//管理员
+        head = AdminAccountHead;
+        path = "C:\\Users\\dyyxs\\Desktop\\SMS\\date\\AdminDate.bin";
+    }
+
+    ifstream ifs(path, ios::binary);
+    if (!ifs) {
+        cout << "打开文件失败" << endl;
+        exit(1);
+    }
+    int multiple;
+    ifs.read((char*)&multiple, sizeof(int));
+    if (multiple > 0) {
+        for (int i = 0; i < multiple; i++) {
+            int length;
+            ifs.read((char*)&length, sizeof(int));
+            string ID(length, '\0');
+            ifs.read((char*)&ID[0], length);
+            ifs.read((char*)&length, sizeof(int));
+            string passWord(length, '\0');
+            ifs.read((char*)&passWord[0], length);
+            AccountNode* temp = new AccountNode(ID, passWord);
+            AccountNode* seek = head;
+            if (!ifs) {
+                cout << "文件读取失败" << endl;
+            }
+            while (NULL != seek->next) {
+                seek = seek->next;
+            }
+            seek->next = temp;
+        }
+    }
+    ifs.close();
+}
+void studentNodeRead(void) {//学生细节信息节点读取
+    ifstream ifs("C:\\Users\\dyyxs\\Desktop\\SMS\\date\\StudentIP.bin", ios::binary);
+    if (!ifs) {
+        cout << "打开文件失败" << endl;
+        exit(1);
+    }
+    int multiple;
+    StudentsNode* head = studentHead;
+    StudentsNode* last = NULL;//防止访问到哨兵节点
+    ifs.read((char*)&multiple, sizeof(int));
+    if (multiple > 0) {
+        for (int i = 0; i < multiple; i++) {
+            int length;
+            ifs.read((char*)&length, sizeof(int));
+            string name(length, '\0');
+            ifs.read((char*)&name[0], length);
+            ifs.read((char*)&length, sizeof(int));
+            string score(length, '\0');
+            ifs.read((char*)&score[0], length);
+            ifs.read((char*)&length, sizeof(int));
+            string number(length, '\0');
+            ifs.read((char*)&number[0], length);
+            ifs.read((char*)&length, sizeof(int));
+            string clss(length, '\0');
+            ifs.read((char*)&clss[0], length);
+            StudentsNode* temp = new StudentsNode(name, score, number, clss);
+            temp->prev = last;
+            if (NULL == last) {
+                studentHead->next = temp;
+            } else {
+                last->next = temp;
+            }
+            last = temp;
+            if (!ifs) {
+                cout << "读取文件失败"  << endl;
+            }
+        }
+    }
+    ifs.close();
+}
+void allRead(void) {
+    teacherNodeRead();//老师细节信息读取
+    studentNodeRead();//学生细节信息读取
+    accountRead(1);//1代表学生，0代表老师,2代表管理员
+    accountRead(0);
+    accountRead(2);
 }
 
 void sleepClean(void) {//延迟清屏函数
@@ -332,6 +451,45 @@ void teacherLogin(void) {
     cleanScreen();
     teacherConfirm();
 }
+void adminOption(void) {
+    cout << "成功登录，开发中" << endl;
+    system("pause");
+}
+void adminLogin(void) {
+    while (1) {
+        printf("请输入您的管理员账号\n");
+        string ID = cinLineString();
+        printf("请输入您的管理员密码\n");
+        string passWord = cinLineString();
+        cleanScreen();
+        AccountNode* temp = AdminAccountHead->next;
+        int flag = 0;
+        while (NULL != temp) {
+            if (temp->ID == ID && temp->passWord == passWord) {
+                flag = 1;
+                break;
+            }
+            temp = temp->next;
+        }
+        if (0 == flag) {
+            cout << "抱歉，您的账号或密码不正确，请输入序号进行操作:" << endl;
+            cout << endl;
+            printf("1. 返回主菜单      其他. 重新输入\n");
+            cout << endl;
+            cout << "（注：“其他”指的是除了“1”以外的其他输入）" << endl;
+            string answer = cinLineString();
+            cleanScreen();
+            if (answer == "1") {
+                return;
+            } else {
+                continue;
+            }
+        } else {
+            adminOption();
+            break;
+        }
+    }
+}
 void printOriginChoice(void) {//初始界面
     printf("       学生管理系统\n");
     printf("-----------****-----------\n");
@@ -350,7 +508,7 @@ void printOriginChoice(void) {//初始界面
     } else if ("3" == firstChoice) {
         teacherLogin();
     } else if ("4" == firstChoice) {
-
+        adminLogin();
     } else if ("5" == firstChoice) {
 
     } else if ("6" == firstChoice) {
@@ -365,19 +523,31 @@ void printOriginChoice(void) {//初始界面
     }
 }
 void originLogin(void) {//初始登入界面
-    allAccountRead();
+    allRead();
     while (1) {
         cleanScreen();
         printOriginChoice();
     }
 }
 
-int main() {/*
-    string filename = "C:\\Users\\dyyxs\\Desktop\\SMS\\date\\StudentIP.bin";
+int main() {
+    
+    string filename = "C:\\Users\\dyyxs\\Desktop\\SMS\\date\\AdminDate.bin";
     ofstream fls(filename, ios::trunc | ios::binary);
-    int te1 = 0;
+    int te1 = 1;
     fls.write((char *)&te1, sizeof(int));
+    int length;
+    string ID = "dyyxs";
+    length = ID.size();
+    fls.write((char*)&length, sizeof(int));
+    fls.write((char*)&ID[0], ID.size());
+    string passWord = "20070806";
+    length = passWord.size();
+    fls.write((char*)&length, sizeof(int));
+    fls.write((char*)&passWord[0], passWord.size());
     fls.close();
+    
+    /*
     ifstream fls1(filename, ios::binary);
     int out;
     fls1.read((char *)&out, sizeof(int));
